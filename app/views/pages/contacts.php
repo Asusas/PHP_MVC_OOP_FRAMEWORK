4e375b219852d078b72e26cfa349384f9efca396
design-s1 <andreliskrug@gmail.com>
2020-04-23 23:53:45 +0300

    contacts.php edited

--- app/views/pages/contacts.php
+++ app/views/pages/contacts.php
@@ -1,7 +1,7 @@
 <?php require APPROOT . '/views/includes/header.php'?>
 
 <form action="<?php echo URLROOT ?>/page/send" method="POST" enctype="multipart/form-data">
-    <div class="col-lg-6 mx-auto">
+    <div class="col-lg-6 mx-auto bg-light card card-body">
         <div><?php echo success_message() ?></div>
         <h5 class="text-center">If you have any questions, please send us an email</h5>
         <hr>
@@ -10,7 +10,7 @@
             <label for="subject">Subject: <sup>*</sup> </label>
             <input type="text" name="subject"
                 class=" form-control form-control-sm <?php echo (!empty($data['subject_error'])) ? 'is-invalid' : ''; ?>"
-                value="<?php echo $data['subject']; ?>">
+                value="<?php echo isset($data['subject']) ? $data['subject'] : ''; ?>">
             <span class="invalid-feedback"><?php echo $data['subject_error'] ?></span>
         </div>
 
@@ -18,7 +18,7 @@
             <label for="subject">Your email: <sup>*</sup> </label>
             <input type="email" name="email"
                 class=" form-control form-control-sm <?php echo (!empty($data['email_error'])) ? 'is-invalid' : ''; ?>"
-                value="<?php echo $data['email']; ?>">
+                value="<?php echo isset($data['email']) ? $data['email'] : ''; ?>">
             <span class="invalid-feedback"><?php echo $data['email_error'] ?></span>
         </div>
 
@@ -26,7 +26,8 @@
             <label for="message">Message: <sup>*</sup> </label>
             <textarea name="message" type="textarea"
                 class=" form-control form-control-sm <?php echo (!empty($data['message_error'])) ? 'is-invalid' : ''; ?>"
-                placeholder="Type your message" rows="7"><?php echo $data['message']; ?></textarea>
+                placeholder="Type your message"
+                rows="7"><?php echo isset($data['message']) ? $data['message'] : ''; ?></textarea>
             <span class="invalid-feedback"><?php echo $data['message_error'] ?></span>
         </div>
 
